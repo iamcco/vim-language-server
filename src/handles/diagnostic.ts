c5f3f0ea39afbb39942497d769441a5cda52af3a
iamcco <ooiss@qq.com>
2020-05-07 11:56:14 +0800

    fix(diagnostic): fix negative position closes #40

--- src/handles/diagnostic.ts
+++ src/handles/diagnostic.ts
@@ -7,19 +7,28 @@ import {
 import { errorLinePattern } from "../common/patterns";
 import { connection } from "../server/connection";
 
+const fixNegtiveNum = (num: number): number => {
+  if (num < 0) {
+    return 0;
+  }
+  return num;
+};
+
 export async function handleDiagnostic(
   textDoc: TextDocument,
   error: string,
 ) {
   const m = (error || "").match(errorLinePattern);
   if (m) {
-   return connection.sendDiagnostics({
+    const line = fixNegtiveNum(parseFloat(m[2]) - 1);
+    const col = fixNegtiveNum(parseFloat(m[3]) - 1);
+    return connection.sendDiagnostics({
       uri: textDoc.uri,
       diagnostics: [{
         message: m[1],
         range: Range.create(
-          Position.create(parseFloat(m[2]) - 1, parseFloat(m[3]) - 1),
-          Position.create(parseFloat(m[2]) - 1, parseFloat(m[3])),
+          Position.create(line, col),
+          Position.create(line, col + 1),
         ),
         severity: DiagnosticSeverity.Error,
       }],
